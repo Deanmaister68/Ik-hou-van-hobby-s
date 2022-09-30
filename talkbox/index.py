57d8e8d0e69da1e1d0d8141220ed589d89868129
Deabmaister68 <514109@vistacollege.nl>
2022-09-30 13:55:20 +0200

    basis talkbox

--- talkbox/index.py
+++ talkbox/index.py
@@ -3,19 +3,22 @@ print("Welkom bij ons evenamentenbureau.")
 name = input("Wat is je naam? ")
 print("Hallo " + name + "," + "dankjewel voor je komst.")
 print()
-menu = "Gitaar \n" + "gamen \n" + "coderen \n"
+asortiment = "workshop gitaar \n" + "workshop gamen \n" + "workshop coderen \n"
 print("Ons aanbod aan workshops:-")
-print(menu)
-customer_food = input("Kies je workshop.: ")
-print("Een" , customer_food  , "is of 10,50EUR")
-order = "voor hoeveel " + customer_food + " personen wilt u deze workshop: "
+print(asortiment)
+workshop = input("Kies je workshop: ")
+print("Een" , workshop  , "is 10EUR pp")
+order = "voor hoeveel personen wilt u de " + workshop + "?: "
 print(order)
+aantal_personen = input()
+aantal_personen = int(aantal_personen)
+
+totaal = 10*aantal_personen
 
 
-
-while customer_food not in menu:
-  print("This food is not available")
-  customer_food = input("Enter your food: ")
+while workshop not in asortiment:
+  print("Deze workshop is niet beschikbaar")
+  workshop = input("Kies workshop: ")
   
-print("Your order is of" , total , "$")
-print("Hey" , name , "we'll have your order of" , number_of_coffee , customer_food , "ready for you!!")
+print("Uw bestelling van" , totaal , "EUR")
+print("Hallo" , name , "we hebben uw bestelling van" , aantal_personen , "personen voor" , workshop , "voor u klaar!")
