c6d216759ea728a2e46d56eb243100547341d121
Deabmaister68 <514109@vistacollege.nl>
2022-10-03 14:58:26 +0200

    talkbox and contact refine

--- talkbox/index.py
+++ talkbox/index.py
@@ -17,9 +17,11 @@ aantal_personen = int(aantal_personen)
 
 totaal = 10*aantal_personen
 
-datum = input("Wanneer wilt u dat deze workshop plaats vind? \n")
+#date/time/place input
+
+datum = input("Welke datum wilt u dat deze workshop plaats vind? \n")
 tijd = input("Hoelaat wilt u dat deze workshop plaats vind? \n")
-plaats = input("Waar wilt u waar deze workshop zig afspeeld \n")
+plaats = input("Welke locatie wilt u dat deze workshop zig afspeeld? \n")
 
 while workshop not in asortiment:
   print("Deze workshop is niet beschikbaar")
@@ -29,8 +31,9 @@ print("Uw bestelling is dan" , totaal , "EUR")
 
 bevesteging = input("Wilt u door gaan met uw bestelling? \n" + "ja of nee \n")
 if bevesteging == "nee" :
-  print("Well fuck you then!")
+  print("Well pissoff then you wanker!")
 else:
   print("Hallo" , name , "we hebben uw bestelling van" , aantal_personen ,
-   "personen voor de workschop" , workshop , "voor", datum , "om" , tijd , "ontvangen!")
+   "personen voor de workschop" , workshop , "voor", datum , "om" , tijd , "ontvangen! \n")
 
+print("Als er nog verdere vragen zijn kunt u ons bereike op onze mail:ikhouvanhobbies@gmail.com of bel onze klantenservice op:0604555456 ")
