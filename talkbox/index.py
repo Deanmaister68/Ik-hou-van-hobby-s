4f700ccb2b61ccc62d65720fceda6d6255375c06
Deabmaister68 <514109@vistacollege.nl>
2022-10-03 10:14:52 +0200

    edit
    Meer opties toegevoegd

--- talkbox/index.py
+++ talkbox/index.py
@@ -1,24 +1,27 @@
 
 print("Welkom bij ons evenamentenbureau.")
-name = input("Wat is je naam? ")
-print("Hallo " + name + "," + "dankjewel voor je komst.")
+name = input("Wat is je naam?\n")
+print("Hallo " + name + ", " + "dankjewel voor je komst.")
 print()
-asortiment = "workshop gitaar \n" + "workshop gamen \n" + "workshop coderen \n"
+asortiment = "gitaar \n" + "gamen \n" + "progameren \n" + "fitnissen \n" + "auto spotten \n" + "wandelen \n"
 print("Ons aanbod aan workshops:-")
 print(asortiment)
 workshop = input("Kies je workshop: ")
-print("Een" , workshop  , "is 10EUR pp")
-order = "voor hoeveel personen wilt u de " + workshop + "?: "
+print("Een " + "workshop" , workshop  , "is 10EUR pp")
+order = "voor hoeveel personen wilt u de workshop " + workshop + "?: "
 print(order)
 aantal_personen = input()
 aantal_personen = int(aantal_personen)
 
 totaal = 10*aantal_personen
 
+datum = input("Wanneer wilt u dat deze workshop plaats vind? \n")
 
 while workshop not in asortiment:
   print("Deze workshop is niet beschikbaar")
   workshop = input("Kies workshop: ")
   
-print("Uw bestelling van" , totaal , "EUR")
-print("Hallo" , name , "we hebben uw bestelling van" , aantal_personen , "personen voor" , workshop , "voor u klaar!")
+print("Uw bestelling is dan" , totaal , "EUR")
+
+bevesteging = input("Wilt u door gaan met uw bestelling? \n" + "ja of nee \n")
+print("Hallo" , name , "we hebben uw bestelling van" , aantal_personen , "personen voor de workschop" , workshop , "voor", datum , "voor u klaar!")
